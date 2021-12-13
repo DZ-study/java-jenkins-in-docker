328badffeaebdad030283c7edec2eda67e249023
janicedong <1213198891@qq.com>
2021-12-13 17:06:00 +0800

    测试自动构建

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,9 +8,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(checkIfInputIsAnEvenNumber(22));
+        testTrigger();
     }
 
     public static boolean checkIfInputIsAnEvenNumber(int number) {
         return number % 2 == 0;
+    }
+
+    public static void testTrigger() {
+        System.out.println("testTrigger...");
     }
 }
